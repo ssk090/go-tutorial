d89c892030062eabad262f5ca40bab8fa6dd2bb7
ssk090 <shivanandasai.38@gmail.com>
2025-12-25 00:57:47 +0530

    Add examples of Go variable declarations and types

--- main.go
+++ main.go
@@ -6,5 +6,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Random number:", rand.Intn(100))
+	var number = rand.Int31n(10)
+	fmt.Println("Random number:", number)
+
+	var a int = 5
+	b := "String" // short hand syntax for declaring variables
+
+	// The `:=` operator is distinct from the regular assignment operator `=`, which requires the variable to be already declared.
+	fmt.Println("a:", a, "b:", b)
+
+	isBoolean := false
+	fmt.Print(isBoolean)
+	// A
+
 }
